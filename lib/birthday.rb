13cba25950a1ce774095211de589ba47d5db6d5a
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-17 03:01:36 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age < 12  || age == 12
-     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
    end
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
