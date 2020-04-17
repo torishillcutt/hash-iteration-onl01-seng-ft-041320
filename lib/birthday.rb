d3ff31d19cf8ec96ea3879e33116afd5686ea2cb
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-17 02:52:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,5 +5,5 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each {|kids_name, age| "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  birthday_kids.each {|kids_name, age| puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
